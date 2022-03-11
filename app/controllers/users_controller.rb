be3ae27ad2ec252ffd7b7fbbc88c1c8515521354
Immanuel Currah <immanueli.currah@gmail.com>
2022-03-11 15:25:35 -0500

    added img url to both users and events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,6 +51,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :password)
+      params.require(:user).permit(:name, :email, :password, :img_url)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_11_184038) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_11_202417) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_11_184038) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "img_url"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
@@ -53,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_11_184038) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "img_url"
   end
 
   add_foreign_key "comments", "events"
