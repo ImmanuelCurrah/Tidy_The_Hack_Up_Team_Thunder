54718116295624bb305096fb9461b0d36bcafb9d
Immanuel Currah <immanueli.currah@gmail.com>
2022-03-11 14:20:32 -0500

    user controller done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   has_many :events, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :participants, dependent: :destroy
+
+  has_secure_password
+
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 3 }
 end
