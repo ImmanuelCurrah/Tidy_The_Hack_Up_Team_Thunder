c4fdb9a89e124f8a80b7e27c58371cf3d67cbad5
nickdbmiller <nickbarrettmiller@gmail.com>
2022-03-12 10:40:35 -0500

    Add link to GitHub repo.

--- client/src/Components/Layout/Footer.jsx
+++ client/src/Components/Layout/Footer.jsx
@@ -7,9 +7,12 @@ export default function Footer() {
                 href="https://github.com/ImmanuelCurrah/Tidy_The_Hack_Up_Team_Thunder"
                 target="_blank"
                 rel="noreferrer"
-                className = "hover:text-emerald-200 hover:-translate-y-2 transform transition focus:outline-none focus:ring focus:ring-offset-2 focus:ring-emerald-100 focus:ring-opacity-50 active:text-lime-50"
             >
-                Team Thunder 2022
+                <h4
+                    className = "transform transition hover:-translate-y-1 hover:text-emerald-200  focus:outline-none focus:ring focus:ring-offset-2 focus:ring-emerald-100 focus:ring-opacity-50 active:text-lime-50"
+                >
+                    Team Thunder 2022
+                </h4>
             </a>
         </footer>
     )
