79643dbb85edb90ac872168cf593d71059f36605
Michael Abebe <michaelabebe@michaels-mbp.myfiosgateway.com>
2022-03-12 11:33:00 -0500

    events addition

--- client/src/Screens/Events/Events.jsx
+++ client/src/Screens/Events/Events.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import {Routes, Route, NavLink} from "react-router-dom"
+import EventAdd from './AddEvent/EventAdd';
 import EventDetails from './EventDetails/EventDetails';
-export default function Events() {
+export default function Events(props) {
+  const {currentUser}=props
   const [eventId, setEventId] = useState(0);
   const [events, setEvents] = useState(["clean sidewalk", "test"]);
   return (
@@ -11,8 +13,11 @@ export default function Events() {
           Home
           <br/>
           <NavLink to={`details/${eventId}`}>detail link</NavLink>
+          <NavLink to={"add"}>Add Event</NavLink>
         </div>} />
         <Route path={`details/${eventId}`} element={<div><EventDetails eventId={eventId} events={events}></EventDetails>
+        </div>} />
+        <Route path={"add"} element={<div><EventAdd currentUser={currentUser} />
         </div>} />
       </Routes>
     </div>
