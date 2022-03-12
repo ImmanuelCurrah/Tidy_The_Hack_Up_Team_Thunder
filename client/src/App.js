1701be417d04ae29ab85ca2161e67510edc4cc6d
Immanuel Currah <immanueli.currah@gmail.com>
2022-03-11 20:35:06 -0500

    user verification and state in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,45 @@
 import "./App.css";
 import Layout from "./Components/Layout";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Home from "./Screens/Home";
 import Signup from "./Screens/Signup";
 import Login from "./Screens/Login";
+import { verifyUser } from "./services/routes/auth-config";
+
 function App() {
-  return <div className="App">
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/login" element={<Login/>}/>
-      </Routes>
-    </Layout>
-  </div>;
+  const [currentUser, setCurrentUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getUser = async () => {
+      const user = await verifyUser();
+      if (user) {
+        setCurrentUser(user);
+      } else {
+        setCurrentUser(null);
+      }
+    };
+    getUser();
+  }, []);
+
+  const logout = () => {
+    localStorage.removeItem("AuthToken");
+    setCurrentUser(null);
+    navigate("/");
+  };
+
+  return (
+    <div className="App">
+      <Layout currentUser={currentUser}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
