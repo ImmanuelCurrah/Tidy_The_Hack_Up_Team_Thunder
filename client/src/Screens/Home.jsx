1e25dffb454ad97a46a1b096069a905e21437287
Immanuel Currah <immanueli.currah@gmail.com>
2022-03-12 15:22:25 -0500

    working on homepage but need events config

--- client/src/Screens/Home.jsx
+++ client/src/Screens/Home.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+import earth from "../assets/earth.png";
 
-export default function Home() {
-  return <div>Home</div>;
+export default function Home(props) {
+  const { currentUser } = props;
+  console.log(currentUser);
+
+  return (
+    <div>
+      {currentUser ? (
+        <div> {`Welcome back ${currentUser.name}`} </div>
+      ) : (
+        <div>signup</div>
+      )}
+      <img src={earth} alt="a purple earth" />
+      <h2>Featured Events!</h2>
+    </div>
+  );
 }
