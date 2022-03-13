0903bfec0523e1c56a2a9ddcf05115cf998faec2
Immanuel Currah <immanueli.currah@gmail.com>
2022-03-12 23:03:35 -0500

    styled the add event form

--- client/src/Screens/Events/AddEvent/EventAdd.jsx
+++ client/src/Screens/Events/AddEvent/EventAdd.jsx
@@ -17,6 +17,7 @@ export default function EventAdd(props) {
   });
 
   const navigate = useNavigate();
+
   const handleChange = (e) => {
     const { id, value } = e.target;
     setEvent((prevInput) => ({
@@ -25,68 +26,115 @@ export default function EventAdd(props) {
     }));
   };
 
-  const handleEvent = async (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-    console.log(event);
     const resp = await createEvent(event);
-    // props.setCurrentUser(resp)
     navigate("/Events");
   };
 
   return (
     <div>
       {props.currentUser ? (
-        <form onSubmit={handleEvent}>
-          <input
-            type="string"
-            id="name"
-            value={event.name}
-            onChange={handleChange}
-            placeholder="name"
-          />
-          <br />
-          <textarea
-            type="string"
-            id="description"
-            value={event.description}
-            onChange={handleChange}
-            placeholder="Description"
-          />
-          <br />
-          <textarea
-            type="string"
-            id="equiptment"
-            value={event.equiptment}
-            onChange={handleChange}
-            placeholder="Equipment"
-          />
-          <br />
-          <input
-            type="number"
-            id="donations"
-            value={event.donations}
-            onChange={handleChange}
-            placeholder="Donation"
-          />
-          <br />
-          <textarea
-            type="string"
-            id="location"
-            value={event.location}
-            onChange={handleChange}
-            placeholder="Location"
-          />
-          <br />
-          <textarea
-            type="string"
-            id="img_url"
-            value={event.img_url}
-            onChange={handleChange}
-            placeholder="Image Url"
-          />
-          <br />
-          <button>Add</button>
-        </form>
+        <section className="flex flex-col items-center m-8 space-y-3">
+          <form
+            onSubmit={onSubmit}
+            className="text-lg text-center p-2 font-noto-display bg-emerald-500 flex flex-col items-center text-emerald-1000
+        rounded-2xl"
+          >
+            <div className="text-emerald-1000">
+              <label className="block">Name of Event</label>
+              <input
+                type="string"
+                id="name"
+                value={event.name}
+                onChange={handleChange}
+                required
+                autoFocus
+                className="text-lg bg-emerald-100 rounded-2xl pl-4 py-3"
+              />
+              <label className="block">Description</label>
+              <textarea
+                type="string"
+                id="description"
+                value={event.description}
+                onChange={handleChange}
+                required
+                className="text-lg bg-emerald-100 rounded-2xl pl-4 py-3"
+              />
+              <label className="block">Equipment Needed</label>
+              <textarea
+                type="string"
+                id="equiptment"
+                value={event.equiptment}
+                onChange={handleChange}
+                required
+                className="text-lg bg-emerald-100 rounded-2xl pl-4 py-3"
+              />
+              <label className="block">Donations Needed</label>
+              <input
+                type="number"
+                id="donations"
+                value={event.donations}
+                onChange={handleChange}
+                required
+                className="text-lg bg-emerald-100 rounded-2xl pl-4 py-3"
+              />
+              <label className="block">Participants Needed</label>
+              <input
+                type="number"
+                id="participants_needed"
+                value={event.participants_needed}
+                onChange={handleChange}
+                required
+                className="text-lg bg-emerald-100 rounded-2xl pl-4 py-3"
+              />
+              <label className="block">Location</label>
+              <input
+                type="string"
+                id="location"
+                value={event.location}
+                onChange={handleChange}
+                required
+                className="text-lg bg-emerald-100 rounded-2xl pl-4 py-3"
+              />
+              <label className="block">Image for Post</label>
+              <input
+                type="string"
+                id="img_url"
+                value={event.img_url}
+                onChange={handleChange}
+                required
+                className="text-lg bg-emerald-100 rounded-2xl pl-4 py-3"
+              />
+              <label className="block">Start Date</label>
+              <input
+                type="date"
+                id="start_date"
+                value={event.start_date}
+                onChange={handleChange}
+                required
+                className="text-lg bg-emerald-100 rounded-2xl pl-4 py-3"
+              />
+              <label className="block">End Date</label>
+              <input
+                type="date"
+                id="end_date"
+                value={event.end_date}
+                onChange={handleChange}
+                required
+                className="text-lg bg-emerald-100 rounded-2xl pl-4 py-3"
+              />
+            </div>
+            <button
+              className="bg-emerald-700 text-emerald-100 hover:text-emerald-50 hover:bg-emerald-600
+          hover:-translate-y-0.5 transform transition
+          focus:outline-none focus:ring focus:ring-offset-2 focus:ring-emerald-300 focus:ring-opacity-50
+          active:text-emerald-200 active:bg-emerald-800 rounded-2xl py-2 px-6 my-6"
+            >
+              Add
+            </button>
+          </form>
+        </section>
       ) : (
         <div className="flex flex-row justify-center">
           <h4>Please login</h4>
