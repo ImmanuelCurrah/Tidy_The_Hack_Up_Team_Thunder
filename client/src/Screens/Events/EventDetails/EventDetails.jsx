d1e2440cabf26d094ff05805bb2d99d234d79ff9
Trung Diec <tdiec00@gmail.com>
2022-03-16 20:45:36 -0400

    pulled from dev

--- client/src/Screens/Events/EventDetails/EventDetails.jsx
+++ client/src/Screens/Events/EventDetails/EventDetails.jsx
@@ -1,32 +1,25 @@
-import { useState } from "react";
-import { useParams } from "react-router-dom";
-import { addParticipant } from "../../../services/routes/event-controller";
+import {useState} from "react"
+import {useParams} from "react-router-dom"
+import {addParticipant} from "../../../services/routes/event-controller"
 
 export default function EventDetails(props) {
-  const { events, currentUser } = props;
-  const { id } = useParams();
-  console.log(id);
-  const [handleComment, setHandleComment] = useState(false);
+  const {events, currentUser} = props
+  const {id} = useParams()
+  console.log(id)
+  const [handleComment, setHandleComment] = useState(false)
   console.log(
     events.filter((e) => Number(e.id) === Number(id)),
     id
-  );
+  )
 
   const handleUpdate = (e) => {
-    console.log("update");
-  };
+    console.log("update")
+  }
 
   const handleParticipate = async () => {
-<<<<<<< HEAD
     const participant = await addParticipant(currentUser.id, id)
-    alert("You have registered for this event")
     console.log(participant)
   }
-=======
-    const participant = await addParticipant(currentUser.id, id);
-    console.log(participant);
-  };
->>>>>>> cfeb932d4dd035d6720597a9a3ba752b5dc47114
   return (
     <div>
       Event Details
@@ -36,21 +29,15 @@ export default function EventDetails(props) {
           .map((e, i) => {
             return (
               <div className="flex flex-col items-center" key={i}>
-                <h2 className="bg-emerald-500 p-2 rounded-full text-emerald-100">
-                  Name:{e.name}
-                </h2>
+                <h2 className="bg-emerald-500 p-2 rounded-full text-emerald-100">Name:{e.name}</h2>
                 {e.img_url !== null && <img src={e.img_url} alt={e.name} />}
                 <div className="bg-emerald-500 mt-2 p-2 rounded-[22px]">
-                  <h2 className="text-emerald-100">
-                    Description:{e.description}
-                  </h2>
+                  <h2 className="text-emerald-100">Description:{e.description}</h2>
                   <h2 className="text-emerald-100">Start:{e.start_date}</h2>
                   <h2 className="text-emerald-100">End:{e.end_date}</h2>
                   <h2 className="text-emerald-100">Equiptment:{e.equipment}</h2>
                   <h2 className="text-emerald-100">Donations:{e.donations}</h2>
-                  <h2 className="text-emerald-100">
-                    Minimum # of Participants:{e.participants_needed}
-                  </h2>
+                  <h2 className="text-emerald-100">Minimum # of Participants:{e.participants_needed}</h2>
                   {e.location && <h1>Location:{e.location}</h1>}
                 </div>
                 <div className="bg-emerald-500 mt-2 p-8 rounded-[28px]">
@@ -60,8 +47,8 @@ export default function EventDetails(props) {
                   <button
                     className="bg-emerald-800 my-2 rounded-md text-emerald-100 p-1"
                     onClick={(e) => {
-                      e.preventDefault();
-                      setHandleComment((prev) => !prev);
+                      e.preventDefault()
+                      setHandleComment((prev) => !prev)
                     }}
                   >
                     Comment
@@ -69,7 +56,7 @@ export default function EventDetails(props) {
                   <button
                     className="bg-emerald-800 my-2 rounded-md text-emerald-100 p-1"
                     onClick={(e) => {
-                      e.preventDefault();
+                      e.preventDefault()
                     }}
                   >
                     Donate
@@ -79,19 +66,15 @@ export default function EventDetails(props) {
                   <div className="bg-emerald-500 mt-2 p-8 rounded-full">
                     <h3>Comment Section</h3>
                     <input></input>
-                    <button className="bg-emerald-800 mx-2 rounded-md text-emerald-100 p-1">
-                      Edit
-                    </button>
-                    <button className="bg-emerald-800 mx-2 rounded-md text-emerald-100 p-1">
-                      Delete
-                    </button>
+                    <button className="bg-emerald-800 mx-2 rounded-md text-emerald-100 p-1">Edit</button>
+                    <button className="bg-emerald-800 mx-2 rounded-md text-emerald-100 p-1">Delete</button>
                   </div>
                 )}
                 <button
                   className="bg-emerald-800 my-2 rounded-md text-emerald-100 p-1"
                   onClick={(e) => {
-                    e.preventDefault();
-                    handleUpdate(e);
+                    e.preventDefault()
+                    handleUpdate(e)
                   }}
                 >
                   Update
@@ -99,15 +82,15 @@ export default function EventDetails(props) {
                 <button
                   className="bg-emerald-800 my-2 rounded-md text-emerald-100 p-1"
                   onClick={(e) => {
-                    e.preventDefault();
-                    handleParticipate(e);
+                    e.preventDefault()
+                    handleParticipate(e)
                   }}
                 >
                   Participate
                 </button>
               </div>
-            );
+            )
           })}
     </div>
-  );
+  )
 }
