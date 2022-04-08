767a530faad94796c2abea2c5ee6baa9892c246b
Immanuel Currah <immanueli.currah@gmail.com>
2022-04-08 10:50:48 -0400

    merging

--- client/src/services/api/api-config/index.js
+++ client/src/services/api/api-config/index.js
@@ -1,7 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 
-const baseURL = process.env.NODE_ENV === "production" ? "https://elegant-lamport-79fb82.netlify.app/" : "http://localhost:3000/"
+// const baseURL =
+//   process.env.NODE_ENV === "production"
+//     ? "https://team-thunder-cleanly.herokuapp.com"
+//     : "http://localhost:3000";
+const baseURL = "https://team-thunder-cleanly.herokuapp.com";
 
 export const api = axios.create({
   baseURL,
-})
+});
