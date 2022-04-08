b7b9db6d0fe3fcc88876fe322159e3253b049676
Immanuel Currah <immanueli.currah@gmail.com>
2022-04-08 10:54:41 -0400

    deploy

--- client/src/Screens/Events/EventDetails/EventDetails.jsx
+++ client/src/Screens/Events/EventDetails/EventDetails.jsx
@@ -1,12 +1,15 @@
-import {useState, useEffect} from "react"
-import {useParams} from "react-router-dom"
-import {addParticipant, showEventParticipants} from "../../../services/routes/event-controller"
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+  addParticipant,
+  showEventParticipants,
+} from "../../../services/routes/event-controller";
 
 export default function EventDetails(props) {
-  const {events, currentUser} = props
-  const {id} = useParams()
-  const [participants, setParticipants] = useState()
-  const [handleComment, setHandleComment] = useState(false)
+  const { events, currentUser } = props;
+  const { id } = useParams();
+  const [participants, setParticipants] = useState();
+  const [handleComment, setHandleComment] = useState(false);
   // console.log(
   //   events.filter((e) => Number(e.id) === Number(id)),
   //   id
@@ -14,21 +17,22 @@ export default function EventDetails(props) {
 
   useEffect(() => {
     const fetchAllParticipants = async () => {
-      const response = await showEventParticipants(id)
-      setParticipants(response)
-    }
-    fetchAllParticipants()
-  }, [])
+      const response = await showEventParticipants(id);
+      setParticipants(response);
+    };
+    fetchAllParticipants();
+    //eslint-disable-next-line
+  }, []);
 
   const handleUpdate = (e) => {
-    console.log("update")
-  }
+    console.log("update");
+  };
 
   const handleParticipate = async () => {
-    await addParticipant(currentUser.id, id)
-    alert("You have registered for this event")
-    window.location.reload(false)
-  }
+    await addParticipant(currentUser.id, id);
+    alert("You have registered for this event");
+    window.location.reload(false);
+  };
   return (
     <div>
       Event Details
@@ -38,15 +42,21 @@ export default function EventDetails(props) {
           .map((e, i) => {
             return (
               <div className="flex flex-col items-center" key={i}>
-                <h2 className="bg-emerald-500 p-2 rounded-full text-emerald-100">Name:{e.name}</h2>
+                <h2 className="bg-emerald-500 p-2 rounded-full text-emerald-100">
+                  Name:{e.name}
+                </h2>
                 {e.img_url !== null && <img src={e.img_url} alt={e.name} />}
                 <div className="bg-emerald-500 mt-2 p-2 rounded-[22px]">
-                  <h2 className="text-emerald-100">Description:{e.description}</h2>
+                  <h2 className="text-emerald-100">
+                    Description:{e.description}
+                  </h2>
                   <h2 className="text-emerald-100">Start:{e.start_date}</h2>
                   <h2 className="text-emerald-100">End:{e.end_date}</h2>
                   <h2 className="text-emerald-100">Equiptment:{e.equipment}</h2>
                   <h2 className="text-emerald-100">Donations:{e.donations}</h2>
-                  <h2 className="text-emerald-100">Minimum # of Participants:{e.participants_needed}</h2>
+                  <h2 className="text-emerald-100">
+                    Minimum # of Participants:{e.participants_needed}
+                  </h2>
                   {e.location && <h1>Location:{e.location}</h1>}
                 </div>
                 <div className="bg-emerald-500 mt-2 p-8 rounded-[28px]">
@@ -56,8 +66,8 @@ export default function EventDetails(props) {
                   <button
                     className="bg-emerald-800 my-2 rounded-md text-emerald-100 p-1"
                     onClick={(e) => {
-                      e.preventDefault()
-                      setHandleComment((prev) => !prev)
+                      e.preventDefault();
+                      setHandleComment((prev) => !prev);
                     }}
                   >
                     Comment
@@ -65,7 +75,7 @@ export default function EventDetails(props) {
                   <button
                     className="bg-emerald-800 my-2 rounded-md text-emerald-100 p-1"
                     onClick={(e) => {
-                      e.preventDefault()
+                      e.preventDefault();
                     }}
                   >
                     Donate
@@ -75,15 +85,19 @@ export default function EventDetails(props) {
                   <div className="bg-emerald-500 mt-2 p-8 rounded-full">
                     <h3>Comment Section</h3>
                     <input></input>
-                    <button className="bg-emerald-800 mx-2 rounded-md text-emerald-100 p-1">Edit</button>
-                    <button className="bg-emerald-800 mx-2 rounded-md text-emerald-100 p-1">Delete</button>
+                    <button className="bg-emerald-800 mx-2 rounded-md text-emerald-100 p-1">
+                      Edit
+                    </button>
+                    <button className="bg-emerald-800 mx-2 rounded-md text-emerald-100 p-1">
+                      Delete
+                    </button>
                   </div>
                 )}
                 <button
                   className="bg-emerald-800 my-2 rounded-md text-emerald-100 p-1"
                   onClick={(e) => {
-                    e.preventDefault()
-                    handleUpdate(e)
+                    e.preventDefault();
+                    handleUpdate(e);
                   }}
                 >
                   Update
@@ -91,14 +105,14 @@ export default function EventDetails(props) {
                 <button
                   className="bg-emerald-800 my-2 rounded-md text-emerald-100 p-1"
                   onClick={(e) => {
-                    e.preventDefault()
-                    handleParticipate(e)
+                    e.preventDefault();
+                    handleParticipate(e);
                   }}
                 >
                   Participate
                 </button>
               </div>
-            )
+            );
           })}
       <p>Participants Enrolled</p>
       {participants &&
@@ -107,8 +121,8 @@ export default function EventDetails(props) {
             <div key={index}>
               <p>{`${index + 1}- ${participant.user_name}`}</p>
             </div>
-          )
+          );
         })}
     </div>
-  )
+  );
 }
